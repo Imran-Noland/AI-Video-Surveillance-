f5513154cb47c5ea5c3047affc48203b3a645351
Imran-Noland <85587020+Imran-Noland@users.noreply.github.com>
2022-06-15 16:45:14 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 status= "";
+objects=[];
+
 function preload(){
 video= createVideo("video.mp4");
 }
@@ -20,4 +22,24 @@ video.volume(0);
 }
 function draw(){
 image(video, 0, 0, 480, 380);
+if(status != ""){
+objectDetector.detect(video, gotResults);
+for(i=0;i<objects.length;i++){
+document.getElementById("status").innerHTML= "Status: Objects Detected";
+document.getElementById("number_of_objects").innerHTML="Number of Objects Detected are:" + objects.length;
+fill("#ff0000");
+percent=floor(objects[i].confidence*100);
+text(objects[i].label+" " + percent + "%", objects[i].x+15, objects[i].y+15);
+noFill();
+stroke("#ff0000");
+rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+}
+}
+}
+function gotResults(error, results){
+if(error){
+console.log(error);
+}
+console.log(results);
+objects= results;
 }
